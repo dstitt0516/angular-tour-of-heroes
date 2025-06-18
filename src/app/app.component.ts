795c19b2504a7136ca5ee68d5c4ab092b5cef317
Dan <dstitt0516@gmail.com>
2025-06-18 10:48:08 -0400

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MessagesComponent } from "./messages/messages.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [MessagesComponent, RouterOutlet, RouterLink, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
+
---
+++ src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes, withViewTransitions()), provideHttpClient(), importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }))]
+};
